2bfeb8a02dfc912dea75769e219ccb94daf22f58
Skrilax_CZ <skrilax@gmail.com>
2015-02-22 23:33:24 +0200

    quark: Add audio_platform_info (kang victara)

--- device.mk
+++ device.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
