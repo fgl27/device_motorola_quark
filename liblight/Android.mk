3c54d0c34f969d30611d590dbeba28c19a34a862
Skrilax_CZ <skrilax@gmail.com>
2015-03-01 22:50:09 +0200

    quark: Fix liblight

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -14,13 +14,14 @@
 # limitations under the License.
 
 LOCAL_PATH:= $(call my-dir)
-
+# HAL module implemenation stored in
+# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_MODULE := lights.quark
+LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Werror
 
