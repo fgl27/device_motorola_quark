72d71bbc3b34806dbcd2b84a8db043a421f1b74a
Skrilax_CZ <skrilax@gmail.com>
2015-03-01 23:58:32 +0200

    quark: Fix RILD, add audio_effects.conf, add TimeService

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -70,6 +70,10 @@ PRODUCT_PACKAGES += \\
     com.qualcomm.services.location \\
     MotGeoFenceSvc
 
+PRODUCT_PACKAGES += \\
+    TimeService \\
+    libtime_genoff
+
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
 
@@ -143,11 +147,23 @@ LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
-LOCAL_MODULE := org.simalliance.openmobileapi
+LOCAL_MODULE := TimeService
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/app/\$(LOCAL_MODULE)/\$(LOCAL_MODULE).apk
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := proprietary/app/\$(LOCAL_MODULE).jar
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := \$(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libtime_genoff
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_MODULE_PATH := \$(TARGET_OUT)/vendor/lib
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include \$(BUILD_PREBUILT)
 
 endif
