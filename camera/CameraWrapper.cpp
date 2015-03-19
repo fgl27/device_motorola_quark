f3ab8fbb458228ae760c0e43c4b38d9d10e29376
Christopher R. Palmer <crpalmer@gmail.com>
2015-03-19 14:46:07 -0400

    quark: camera: Add 4K video recording support
    Change-Id: Ie96cbd8ffaf731c9821461564fc157cd0494bcc8

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -106,6 +106,11 @@ static char *camera_fixup_getparams(int id __attribute__((unused)),
     params.dump();
 #endif
 
+    const char *pf = params.get(android::CameraParameters::KEY_PREVIEW_FORMAT);
+    if (pf && strcmp(pf, "nv12-venus") == 0) {
+        params.set(android::CameraParameters::KEY_PREVIEW_FORMAT, "yuv420sp");
+    }
+
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
     params.dump();
