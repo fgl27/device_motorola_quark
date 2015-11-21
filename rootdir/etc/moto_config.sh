61f41c94279be753b6eb2362dc92aa849be2b8ed
bhb27 <fella.motog@gmail.com>
2015-11-21 08:26:55 -0200

    moto_config.sh
    Signed-off-by: Felipe L. BHB27 <fglfgl27@gmail.com>

--- rootdir/etc/moto_config.sh
+++ rootdir/etc/moto_config.sh
@@ -98,4 +98,6 @@ setprop "ro.product.model" "Moto XT1225 ($CARRIER)"
 setprop "ro.product.display" "Moto Maxx"
 setprop "ro.build.description" "quark_retla-user 5.0.2 LXG22.33-12.16 16 release-keys"
 setprop "ro.build.fingerprint" "motorola/quark_retla/quark_umts:5.0.2/LXG22.33-12.16/16:user/release-keys"
+setprop "ro.telephony.default_network" "9"
+setprop "telephony.lteOnGsmDevice" "1"
 exit 1
