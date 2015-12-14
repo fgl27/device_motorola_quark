544c52b22a2d3225f5742ca9d327eaff644a46ae
Skrilax_CZ <skrilax@gmail.com>
2015-12-13 18:55:56 -0800

    quark: Make sure that dex2oat will not use swap file
    Eventhough we have 3 GB, just in case.

--- device.mk
+++ device.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
     mbhc.bin \
     wcd9310_anc.bin
 
+# Art
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=false
+
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
