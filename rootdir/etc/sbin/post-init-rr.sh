c7b1ff302267302ae6aa181516da3d59386bcfd4
bhb27 <fglfgl27@gmail.com>
2016-09-11 15:15:29 -0300

    Quark: root:init remove not used set ims services use post-init to disable it on non VZW devices
    Signed-off-by: bhb27 <fglfgl27@gmail.com>

--- rootdir/etc/sbin/post-init-rr.sh
+++ rootdir/etc/sbin/post-init-rr.sh
@@ -10,6 +10,9 @@ chmod -R 777 /tmp/
 chmod 6755 /sbin/*
 chmod 6755 /system/xbin/*
 
+# give su root:root to adb su work
+chown root:root /system/xbin/su
+
 # Make tmp folder
 if [ -e /tmp ]; then
 	echo "tmp already exist"
@@ -25,6 +28,18 @@ fi
 if [ -e /system/app/Adaway/lib/arm/libtcpdump_exec.so ]; then
 	chmod 755 /system/app/Adaway/lib/arm/libtcpdump_exec.so
 fi
+
+fsgid=`getprop ro.boot.fsg-id`;
+
+if  [ "$fsgid" != verizon ]; then
+	# stop IMS services Not need for others then VZW users
+	stop imsqmidaemon;
+	stop imsdatadaemon;
+	echo "services stop okay device fsgid = $fsgid" >> /tmp/bootcheck.txt;
+
+else
+	echo "services not stoped for fsgid = $fsgid" >> /tmp/bootcheck.txt;
+fi;
 
 echo "post-init-ROM Boot initiated on $(date)" >> /tmp/bootcheck.txt
 
