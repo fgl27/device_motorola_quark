3a6ab71a40386448b04e72f7713376fc3efdb677
bhb27 <fglfgl27@gmail.com>
2016-10-19 12:50:11 -0200

    Quark: root add isu.sh support
    Signed-off-by: bhb27 <fglfgl27@gmail.com>

--- rootdir/etc/sbin/post-init-rr.sh
+++ rootdir/etc/sbin/post-init-rr.sh
@@ -18,6 +18,19 @@ if [ -e /system/app/Adaway/lib/arm/libtcpdump_exec.so ]; then
 	chmod 755 /system/app/Adaway/lib/arm/libtcpdump_exec.so
 fi
 
+# Isu support
+if [ -e /system/bin/temp_su ]; then
+	mv /system/bin/temp_su /system/bin/su
+fi
+
+if [ -e /system/xbin/isu ]; then
+	mv /system/xbin/isu /system/xbin/su
+	if [ ! -e /system/bin/su ]; then
+		ln -s -f /system/xbin/su /system/bin/su
+	fi
+# Isu end
+fi
+
 # give su root:root to adb su work
 if [ -e /system/xbin/su ]; then
 	chown root:root /system/xbin/su
@@ -38,5 +51,6 @@ fi;
 
 echo "post-init-ROM Boot initiated on $(date)" >> /data/tmp/bootcheck.txt
 umount /system;
+setenforce 1
 exit
 
