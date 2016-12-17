fc41b2173817af923e8a05ce2367c2af43bc295e
Felipe Leon <fglfgl27@gmail.com>
2016-12-17 16:14:08 -0200

    Quark: build libshim thermal and wvm
    Change-Id: I0edc5f7537478c8986e131bc9ded6a044c7c751c
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -327,3 +327,8 @@ PRODUCT_COPY_FILES += \
     kernel/motorola/apq8084/drivers/staging/qcacld-2.0/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat \
     kernel/motorola/apq8084/drivers/staging/qcacld-2.0/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     kernel/motorola/apq8084/drivers/staging/qcacld-2.0/firmware_bin/WCNSS_qcom_wlan_nv.bin:system/etc/wifi/WCNSS_qcom_wlan_nv.bin
+
+# shims
+PRODUCT_PACKAGES += \
+    libshims_thermal \
+    libshims_wvm
