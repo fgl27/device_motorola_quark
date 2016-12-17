0b7990000e7e951b6aded32b8b655b31c89b6cee
Felipe Leon <fglfgl27@gmail.com>
2016-12-17 17:46:29 -0200

    Quark: drop lib shim wvm
    too much shim to make update vedor with latest 7.1.1
    
    Change-Id: I36e67f7f7519195d37bf20cbbeca309956701fef
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -20,11 +20,3 @@ LOCAL_SRC_FILES := thermal.c
 LOCAL_MODULE := libshims_thermal
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := wvm.c
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-LOCAL_MODULE := libshims_wvm
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
