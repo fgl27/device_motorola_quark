bb6d05ac0b347df55b3a52bece13092959abb574
Felipe Leon <fglfgl27@gmail.com>
2017-01-01 19:09:26 -0200

    Quark: use init to set ro.alarm_boot prop
    Change-Id: I81eb6bab78268e201fa5c81fac1039b83d0af49c
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- init/init_quark.cpp
+++ init/init_quark.cpp
@@ -27,12 +27,75 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
+
+#include "init_apq8084.h"
+
+__attribute__ ((weak))
+void init_target_properties()
+{
+}
+
+static int read_file2(const char *fname, char *data, int max_size)
+{
+    int fd, rc;
+
+    if (max_size < 1)
+        return 0;
+
+    fd = open(fname, O_RDONLY);
+    if (fd < 0) {
+        ERROR("failed to open '%s'\n", fname);
+        return 0;
+    }
+
+    rc = read(fd, data, max_size - 1);
+    if ((rc > 0) && (rc < max_size))
+        data[rc] = '\0';
+    else
+        data[0] = '\0';
+    close(fd);
+
+    return 1;
+}
+
+static void init_alarm_boot_properties()
+{
+    char const *alarm_file = "/proc/sys/kernel/boot_reason";
+    char buf[64];
+
+    if (read_file2(alarm_file, buf, sizeof(buf))) {
+        /*
+         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+         * For existing PMIC chips, the following mapping applies
+         * for the value of boot_reason:
+         *
+         * 0 -> unknown
+         * 1 -> hard reset
+         * 2 -> sudden momentary power loss (SMPL)
+         * 3 -> real time clock (RTC)
+         * 4 -> DC charger inserted
+         * 5 -> USB charger insertd
+         * 6 -> PON1 pin toggled (for secondary PMICs)
+         * 7 -> CBLPWR_N pin toggled (for external power supply)
+         * 8 -> KPDPWR_N pin toggled (power key pressed)
+         */
+        if (buf[0] == '3')
+            property_set("ro.alarm_boot", "true");
+        else
+            property_set("ro.alarm_boot", "false");
+    }
+}
 
 void vendor_load_properties()
 {
@@ -43,6 +106,10 @@ void vendor_load_properties()
     std::string radio;
     std::string cid;
     std::string camera_enable_vpu;
+
+
+    init_target_properties();
+    init_alarm_boot_properties();
 
     platform = property_get("ro.board.platform");
     if (platform != ANDROID_TARGET)
