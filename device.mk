b33ec7f7dc51b69bc7f9e40cfbb3514810ec6f5a
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    Quark: media: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    This conversion is needed for Treble OMX, which also
    requires moving the XML to TARGET_COPY_OUT_VENDOR.
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -234,7 +234,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/vendor/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/vendor/etc/media_codecs_performance.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/vendor/etc/media_codecs_google_audio.xml \
