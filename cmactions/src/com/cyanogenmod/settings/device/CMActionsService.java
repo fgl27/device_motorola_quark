4f7061a0cbb101b355d2d6d185a26dd27d02ae62
Felipe Leon <fglfgl27@gmail.com>
2017-05-10 09:33:26 -0300

    Revert "cmactions: Hold a wakelock when the screen is on"
    This reverts commit e993cd5ae598be54b11ee4f22151bbb1a85f4465.

--- cmactions/src/com/cyanogenmod/settings/device/CMActionsService.java
+++ cmactions/src/com/cyanogenmod/settings/device/CMActionsService.java
@@ -35,7 +35,6 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
     private final DozePulseAction mDozePulseAction;
     private final IrGestureManager mIrGestureManager;
     private final PowerManager mPowerManager;
-    private final PowerManager.WakeLock mWakeLock;
     private final ScreenReceiver mScreenReceiver;
     private final SensorHelper mSensorHelper;
 
@@ -76,7 +75,6 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
         mUpdatedStateNotifiers.add(new LiftToSilence(cmActionsSettings, context, mSensorHelper));
 
         mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "CMActionsWakeLock");
         updateState();
     }
 
@@ -100,14 +98,8 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
 
     public void updateState() {
         if (mPowerManager.isInteractive()) {
-            if (!mWakeLock.isHeld()) {
-                mWakeLock.acquire();
-            }
             screenTurnedOn();
         } else {
-            if (mWakeLock.isHeld()) {
-                mWakeLock.release();
-            }
             screenTurnedOff();
         }
         for (UpdatedStateNotifier notifier : mUpdatedStateNotifiers) {
