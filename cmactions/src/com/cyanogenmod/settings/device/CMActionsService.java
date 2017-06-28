5fce1049bb870d36b3468134e4c2f8451f8a4250
Felipe Leon <fglfgl27@gmail.com>
2017-06-28 11:31:17 -0300

    Revert "Quark: cmactions remove some wakelock"
    This reverts commit 3406c7253787ff6d078323c3343f15363433b094.

--- cmactions/src/com/cyanogenmod/settings/device/CMActionsService.java
+++ cmactions/src/com/cyanogenmod/settings/device/CMActionsService.java
@@ -35,6 +35,7 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
     private final DozePulseAction mDozePulseAction;
     private final IrGestureManager mIrGestureManager;
     private final PowerManager mPowerManager;
+    private final PowerManager.WakeLock mWakeLock;
     private final ScreenReceiver mScreenReceiver;
     private final SensorHelper mSensorHelper;
 
@@ -75,6 +76,7 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
         mUpdatedStateNotifiers.add(new LiftToSilence(cmActionsSettings, context, mSensorHelper));
 
         mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "CMActionsWakeLock");
         updateState();
     }
 
@@ -84,6 +86,10 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
 
     @Override
     public void screenTurnedOn() {
+        if (mWakeLock != null && !mWakeLock.isHeld()) {
+            mWakeLock.setReferenceCounted(false);
+            mWakeLock.acquire();
+        }
         for (ScreenStateNotifier screenStateNotifier : mScreenStateNotifiers) {
             screenStateNotifier.screenTurnedOn();
         }
@@ -91,6 +97,9 @@ public class CMActionsService extends IntentService implements ScreenStateNotifi
 
     @Override
     public void screenTurnedOff() {
+        if (mWakeLock != null && mWakeLock.isHeld()) {
+            mWakeLock.release();
+        }
         for (ScreenStateNotifier screenStateNotifier : mScreenStateNotifiers) {
             screenStateNotifier.screenTurnedOff();
         }
