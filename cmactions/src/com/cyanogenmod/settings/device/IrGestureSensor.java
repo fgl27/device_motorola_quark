651ac0e3ad04be98da383490f540b7d0cb182c5a
Felipe Leon <fglfgl27@gmail.com>
2017-06-28 15:12:45 -0300

    Quark: cmactions prevent crash of Irgestures sensor
    On Quark, if you contantly enable and disable the irgestures after some attempts (is random) it will get "lost" and will not work properly after until a reboot.
    Prevent from disable it after hand wave is enable, only send a sensor action if the screen is off.
    
    Change-Id: I02366649fd856bb0bd03d25eaca74848b09daf8d
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- cmactions/src/com/cyanogenmod/settings/device/IrGestureSensor.java
+++ cmactions/src/com/cyanogenmod/settings/device/IrGestureSensor.java
@@ -34,7 +34,7 @@ public class IrGestureSensor implements ScreenStateNotifier, SensorEventListener
     private final IrGestureVote mIrGestureVote;
     private final Sensor mSensor;
 
-    private boolean mEnabled;
+    private boolean mEnabled, mScreenOn;
 
     public IrGestureSensor(CMActionsSettings cmActionsSettings, SensorHelper sensorHelper,
                 SensorAction action, IrGestureManager irGestureManager) {
@@ -49,7 +49,18 @@ public class IrGestureSensor implements ScreenStateNotifier, SensorEventListener
 
     @Override
     public void screenTurnedOn() {
-        if (mEnabled) {
+        mScreenOn = true;
+    }
+
+    @Override
+    public void screenTurnedOff() {
+        mScreenOn = false;
+        if (mCMActionsSettings.isIrWakeupEnabled() && !mEnabled) {
+            Log.d(TAG, "Enabling");
+            mSensorHelper.registerListener(mSensor, this);
+            mIrGestureVote.voteForSensors(IR_GESTURES_FOR_SCREEN_OFF);
+            mEnabled = true;
+        } else if (!mCMActionsSettings.isIrWakeupEnabled() && mEnabled) {
             Log.d(TAG, "Disabling");
             mSensorHelper.unregisterListener(this);
             mIrGestureVote.voteForSensors(0);
@@ -58,24 +69,15 @@ public class IrGestureSensor implements ScreenStateNotifier, SensorEventListener
     }
 
     @Override
-    public void screenTurnedOff() {
-        if (mCMActionsSettings.isIrWakeupEnabled() && !mEnabled) {
-            Log.d(TAG, "Enabling");
-            mSensorHelper.registerListener(mSensor, this);
-            mIrGestureVote.voteForSensors(IR_GESTURES_FOR_SCREEN_OFF);
-            mEnabled = true;
-        }
-    }
-
-    @Override
     public void onSensorChanged(SensorEvent event) {
         int gesture = (int) event.values[1];
 
-        if (gesture == IR_GESTURE_SWIPE || gesture == IR_GESTURE_APPROACH)
+        if (!mScreenOn && (gesture == IR_GESTURE_SWIPE || gesture == IR_GESTURE_APPROACH))
             mSensorAction.action();
     }
 
     @Override
     public void onAccuracyChanged(Sensor mSensor, int accuracy) {
     }
+
 }
