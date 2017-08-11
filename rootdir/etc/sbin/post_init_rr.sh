dbef66a46752aaa89f2466b637bd55467d895fd2
Felipe Leon <fglfgl27@gmail.com>
2017-08-11 14:10:20 -0300

    Quark: update init cleanup
    Change-Id: I0511895fb18b9141ff590f57ddd84630b9c69901
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- rootdir/etc/sbin/post_init_rr.sh
+++ rootdir/etc/sbin/post_init_rr.sh
@@ -51,24 +51,27 @@ fi
 ## Clean Verizon blobs on others devices
 if [ "$clean" == 1 ]; then
 
+mount -o rw,remount /system
+
 	# stop IMS services Not need for others then VZW users * have disable volte.rc when they are enable remove the #
 #	stop imsqmidaemon;
 #	stop imsdatadaemon;
 #	setprop net.lte.volte_call_capable false
 
 	# delete main folders
-	app="system/app";
-	bin="system/bin";
-	etc="system/etc/permissions";
-	framework="system/framework";
-	lib="system/lib";
-	priv_app="system/priv-app";
-	vendor_lib="system/vendor/lib";
+	app="/system/app";
+	bin="/system/bin";
+	etc="/system/etc/permissions";
+	framework="/system/framework";
+	lib="/system/lib";
+	priv_app="/system/priv-app";
+	vendor_lib="/system/vendor/lib";
 
 	# delete vzw only files
 	for FILE in $app/ims $app/RCSBootstraputil $app/RcsImsBootstraputil $app/VZWAPNLib $priv_app/AppDirectedSMSProxy $priv_app/BuaContactAdapter $priv_app/VZWAPNService $bin/imsdatadaemon $bin/ims_rtp_daemon $bin/imsqmidaemon $etc/com.verizon.hardware.telephony.ehrpd.xml $etc/com.verizon.hardware.telephony.lte.xml $etc/com.verizon.ims.xml  $etc/rcsservice.xml $etc/rcsimssettings.xml $etc/com.motorola.DirectedSMSProxy.xml $etc/com.vzw.vzwapnlib.xml $framework/com.motorola.ims.rcsmanager.jar $framework/com.verizon.hardware.telephony.ehrpd.jar $framework/com.verizon.hardware.telephony.lte.jar $framework/com.verizon.hardware.telephony.srlte.jar $framework/com.verizon.ims.jar $framework/rcsimssettings.jar $framework/rcsservice.jar $lib/libimscamera_jni.so $lib/libimsmedia_jni.so $vendor_lib/lib-dplmedia.so $vendor_lib/lib-ims-setting-jni.so $vendor_lib/lib-ims-settings.so $vendor_lib/lib-imsSDP.so $vendor_lib/lib-imsdpl.so $vendor_lib/lib-imsqimf.so $vendor_lib/lib-imsrcs.so $vendor_lib/lib-imss.so $vendor_lib/lib-imsvt.so $vendor_lib/lib-imsxml.so $vendor_lib/lib-rcsimssjni.so $vendor_lib/lib-rcsjni.so $vendor_lib/lib-rtpcommon.so $vendor_lib/lib-rtpcore.so $vendor_lib/lib-rtpdaemoninterface.so $vendor_lib/lib-rtpsl.so $vendor_lib/libvcel.so; do
 
-		if [ -e "$FILE" ]; then 
+		if [ -e "$FILE" ]; then
+			echo 'post_init: delete' $FILE > /dev/kmsg;
 			rm -rf $FILE;
 		fi;
 
@@ -85,4 +88,3 @@ mount -o ro,remount /system;
 umount /system;
 
 exit
-
