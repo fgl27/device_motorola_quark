890e0098f86d51b41c7db3721afdfed776605733
Adrian DC <radian.dc@gmail.com>
2017-08-22 11:57:00 +0200

    Quark: Upgrade camera library dependencies for AOSP 8.0.0
    Change-Id: If04e62bbf0905534c03438f95458f0d6c5045ae2
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,16 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/media/camera/include
+LOCAL_C_INCLUDES := \
+    framework/native/include \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
-LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl \
-    android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libutils \
+    android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 LOCAL_HEADER_LIBRARIES := libnativebase_headers
 
@@ -20,4 +28,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
-#include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
