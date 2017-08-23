bb759ffa929e76179fc775940baed6699c324979
Simon Shields <simon@lineageos.org>
2017-08-24 00:23:09 +1000

    Quark; camerawrapper: Add missing libs for headers
    Change-Id: Ifdaf82855e9ccc3b9d8e13f4124e3670b803109d
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -6,8 +6,11 @@ LOCAL_C_INCLUDES := system/media/camera/include
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
+LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libutils libcutils libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
