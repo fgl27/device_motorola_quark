9752bdd216e5faa2da44d98cbefece64d68103d0
Felipe Leon <fglfgl27@gmail.com>
2017-09-15 16:58:02 -0300

    Quark: libligth don't disable charging led if enable
    Change-Id: Id058dc6bbd1dde672e0e2bcfa4a0e2116d215ea4
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- liblight/lights.c
+++ liblight/lights.c
@@ -20,15 +20,19 @@
 
 #include <cutils/log.h>
 
+#include <fcntl.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #include <sys/ioctl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 #include <hardware/lights.h>
@@ -55,6 +59,9 @@ char const*const LED_BLINK
 char const*const LED_BRIGHTNESS
         = "/sys/class/leds/charging/brightness";
 
+char const*const LED_TRIGER
+        = "/sys/class/leds/charging/trigger";
+
 /**
  * device methods
  */
@@ -68,6 +75,29 @@ void init_globals(void)
     memset(&g_battery, 0, sizeof(g_battery));
     memset(&g_notification, 0, sizeof(g_notification));
     memset(&g_attention, 0, sizeof(g_attention));
+}
+
+static int read_file2(const char* fname, char *data, int max_size)
+{
+    int fd, rc;
+
+    if (max_size < 1)
+        return 0;
+
+    fd = open(fname, O_RDONLY);
+    if (fd < 0) {
+        ALOGE("failed to open '%s'\n", fname);
+        return 0;
+    }
+
+    rc = read(fd, data, max_size - 1);
+    if ((rc > 0) && (rc < max_size))
+        data[rc] = '\0';
+    else
+        data[0] = '\0';
+    close(fd);
+
+    return 1;
 }
 
 static int
@@ -154,6 +184,9 @@ set_speaker_light_locked(struct light_device_t* dev,
 {
     unsigned long onMS, offMS;
     char blink_string[PAGE_SIZE];
+    char buf[256];
+    char *batfull = "[battery-full]";
+    read_file2(LED_TRIGER, buf, sizeof buf);
 
     switch (state->flashMode) {
         case LIGHT_FLASH_TIMED:
@@ -175,6 +208,10 @@ set_speaker_light_locked(struct light_device_t* dev,
 
     int brightness = rgb_to_brightness(state);
     write_int(LED_BRIGHTNESS, brightness);
+
+    if(strstr(buf, batfull) != NULL) {
+        write_str(LED_TRIGER, "battery-full");
+    }
     return 0;
 }
 
