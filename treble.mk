b3c72baa2944f7199b230be795beb1ad867f022e
micky387 <mickaelsaibi@free.fr>
2018-02-04 13:34:17 +0100

    Quark: treble: add few HIDL
    -Passthrough:
    *Audio
    *Bluetooth
    *Camera
    *Drm
    *Gnss (Gps)
    *Keymaster
    *Graphics: composer,mapper
    *Light
    *Memtrack
    *Nfc
    *Power
    *Radio
    *Renderscript
    *Sensors
    *Thermal
    *Vibrator
    
    -HwBinder
    *Graphics: configstore & allocator
    *Radio
    *Usb
    *Wifi
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- treble.mk
+++ treble.mk
@@ -14,6 +14,73 @@
 # limitations under the License.
 #
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
+# Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
+# IR Blaster
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
+# Wifi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service
