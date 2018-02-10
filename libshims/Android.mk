a8e67fbdb5cad5bc371bd3e939cc2ac7415327ec
Felipe Leon <fglfgl27@gmail.com>
2018-02-10 03:49:55 -0200

    Quark: Move shims to verdor
    Change-Id: Ifa08166964f04cb947a71d302fd588a692ca19c4
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -19,6 +19,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := thermal.c
 LOCAL_MODULE := libshims_thermal
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 # libqc-opt
@@ -27,4 +28,5 @@ LOCAL_SRC_FILES := icu51.c
 LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
 LOCAL_MODULE := libshim_qcopt
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
