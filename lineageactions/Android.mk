556fa9e5c72f4b4b12b421ed2530908451248faa
Felipe Leon <fglfgl27@gmail.com>
2018-02-24 08:37:11 -0300

    Quark: lineageactions add original app name and remove not use import
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- lineageactions/Android.mk
+++ lineageactions/Android.mk
@@ -16,9 +16,6 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-preference \
     android-support-v7-recyclerview
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
