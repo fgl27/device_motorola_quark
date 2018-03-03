723243ffdf3f7ba2512bea7d7bc22370d2d6669b
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    Quark: Migrate audio V4
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Merged-In: Ife559125850277865451f92ea862e7ec47fc9da7
    Cherry-picked from master
    Signed-off-by: Kevin Rocard <krocard@google.com>
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -16,8 +16,8 @@
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
