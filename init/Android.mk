fcd4301be0d46b74c2589a7b4dfc619908bc151d
Corinna Vinschen <xda@vinschen.de>
2018-03-08 13:48:03 +0100

    Quark: add selinux include path to init dir
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := system/core/init external/selinux/libselinux/include
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_apq8084.cpp
 LOCAL_MODULE := libinit_apq8084
