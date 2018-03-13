7e627d1c072708e26d344270acc940523e1c7213
Hema Iyer Sankaranarayanan <hsankara@codeaurora.org>
2018-03-13 08:56:24 +0100

    Quark: gps: Handle race condition in HAL post SSR
    Post SSR, ensure necessary config injection is done before restarting
    an existing tracking session
    
    Change-Id: I1bec8ff1cab3a9762e73a13958fe91c62f40fb92
    CRs-fixed: 888187
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- gps/loc_api/libloc_api_50001/loc_eng.cpp
+++ gps/loc_api/libloc_api_50001/loc_eng.cpp
@@ -2836,7 +2836,7 @@ void loc_eng_handle_engine_up(loc_eng_data_s_type &loc_eng_data)
     if (loc_eng_data.adapter->isInSession()) {
         // This sets the copy in adapter to modem
         loc_eng_data.adapter->setInSession(false);
-        loc_eng_start_handler(loc_eng_data);
+        loc_eng_data.adapter->sendMsg(new LocEngStartFix(loc_eng_data.adapter));
     }
 
     EXIT_LOG(%s, VOID_RET);
