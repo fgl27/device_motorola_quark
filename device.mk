fe2ec066b7213b3febb5a15d66f1e15d6b4ea555
Corinna Vinschen <xda@vinschen.de>
2018-04-01 21:07:14 +0200

    Quark: add missing audio_effects.xml
    Also add (but don't install) stock audio_effects.conf for future reference.
    
    Our current XML is basically stock without the proprietary libs.
    
    Change-Id: I1a1869f4af861db20758f7330586fd0e1287b8b1
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>
    
    o
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -151,7 +151,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/vendor/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml	
