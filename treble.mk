3938124ce9fc2987060b3690743c96e556317410
Bruno Martins <bgcngm@gmail.com>
2018-06-01 20:53:33 +0200

    Quark: Include DRM service
    Change-Id: I76eccf8e0741fb7f756508f1f9cffafc3c8b7891
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- treble.mk
+++ treble.mk
@@ -31,7 +31,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Graphics
 PRODUCT_PACKAGES += \
