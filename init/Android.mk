a194667b60098d7f77292ec1c00c545d063d72fa
Felipe Leon <fglfgl27@gmail.com>
2018-06-26 11:25:37 -0300

    Quark: libinit_quark to libinit_apq8084
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_quark.cpp
-LOCAL_MODULE := libinit_quark
+LOCAL_SRC_FILES := init_apq8084.cpp
+LOCAL_MODULE := libinit_apq8084
 
 include $(BUILD_STATIC_LIBRARY)
