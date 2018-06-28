3ee7b618ef7ea77220429743c5a3b4377836838d
Felipe Leon <fglfgl27@gmail.com>
2018-06-28 08:54:34 -0300

    QUark: treble update camera config
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- treble.mk
+++ treble.mk
@@ -26,8 +26,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl
 
 # DRM
 PRODUCT_PACKAGES += \
