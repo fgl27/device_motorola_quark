a41278fc8676f0cc934012c28b00b72372c0a28b
Felipe Leon <fglfgl27@gmail.com>
2018-06-29 12:54:40 -0300

    Quark: update config.fs
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- configs/config.fs
+++ configs/config.fs
@@ -1,3 +1,12 @@
+[AID_QCOM_DIAG]
+value:2950
+
+[AID_RFS]
+value:2951
+
+[AID_RFS_SHARED]
+value:2952
+
 [system/bin/qmuxd]
 mode: 0700
 user: AID_RADIO
@@ -9,3 +18,21 @@ mode: 0700
 user: AID_CAMERA
 group: AID_SHELL
 caps: SYS_NICE
+
+[system/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
+
+[system/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE NET_RAW NET_ADMIN
+
+[system/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE NET_RAW NET_ADMIN
