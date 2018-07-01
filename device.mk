f122a3b2b1c6b8acb50b883997020af9fc48bf9e
Felipe Leon <fglfgl27@gmail.com>
2018-07-01 14:17:55 -0300

    Quark: pack Jelly
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -146,6 +146,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Browser
+PRODUCT_PACKAGES += \
+    Jelly
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.apq8084
