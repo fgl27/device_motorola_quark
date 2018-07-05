2ae3a9006b69cd34fdfb7a05e9ae6555e7b93f07
Felipe Leon <fglfgl27@gmail.com>
2018-07-04 22:04:06 -0300

    Quark: start zram in init.sh
    for some reason, sepolicy probably swapon_all is not working
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- rootdir/etc/sbin/post_init_rr_bootc.sh
+++ rootdir/etc/sbin/post_init_rr_bootc.sh
@@ -1,4 +1,13 @@
 #!/system/bin/sh
 mount -o ro,remount /system;
 echo 'post_init_c: run OK' > /dev/kmsg;
+
+# Start zram
+echo 0 > /proc/sys/vm/page-cluster
+echo 25 > /proc/sys/vm/swappiness
+#size is 25% = 768 = 768 * 1024 * 1024 = 805306368
+echo 805306368 > /sys/block/zram0/disksize
+mkswap /dev/block/zram0
+swapon /dev/block/zram0
+
 exit
