087c2072e42e6661a5fbc14646faea6a1250e4ac
Felipe Leon <fglfgl27@gmail.com>
2018-07-06 12:50:05 -0300

    Quark: Use camera impl-legacy and reset CameraWrapper changes
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- treble.mk
+++ treble.mk
@@ -26,8 +26,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy
 
 # DRM
 PRODUCT_PACKAGES += \
