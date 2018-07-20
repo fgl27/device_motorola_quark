d4e63a049a1b385f85624cb446ba98a0e8e8ac2f
Felipe Leon <fglfgl27@gmail.com>
2018-07-20 10:47:46 -0300

    Quark: switch to lineage vibrator service
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- treble.mk
+++ treble.mk
@@ -81,7 +81,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.lineage
 
 # Wifi
 PRODUCT_PACKAGES += \
