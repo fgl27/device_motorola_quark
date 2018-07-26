ac3b592e3497c15b2c899b802d7607edea1e0989
Felipe Leon <fglfgl27@gmail.com>
2018-07-26 09:41:02 -0300

    Quark: set gpu max power level
    in relation to max pwr level
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- rootdir/etc/sbin/post.init.rr.bootc.sh
+++ rootdir/etc/sbin/post.init.rr.bootc.sh
@@ -10,4 +10,12 @@ echo 805306368 > /sys/block/zram0/disksize
 mkswap /dev/block/zram0
 swapon /dev/block/zram0
 
+pwl=`cat /sys/devices/fdb00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/num_pwrlevels`;
+
+if [ "$pwl" == "9" ]; then
+	echo 3 > /sys/devices/fdb00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/max_pwrlevel
+else
+	echo 1 > /sys/devices/fdb00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/max_pwrlevel
+fi
+
 exit
