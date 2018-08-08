c5b978614f40efb014906abf30c3de95416d7607
Felipe Leon <fglfgl27@gmail.com>
2018-08-08 10:21:23 -0300

    Quark: don't pack audio_ext_spkr
    not neded
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -135,7 +135,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
