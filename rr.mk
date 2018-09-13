d7f34089714872fe23a9f5a669a31cf7aac7f112
Felipe Leon <fglfgl27@gmail.com>
2018-09-12 23:02:53 -0300

    Quark: update ro.lineage.version prop
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- rr.mk
+++ rr.mk
@@ -7,5 +7,12 @@ PRODUCT_NAME := rr_quark
 # export the below before . build/envsetup.sh
 # export ROM_VVV=$(grep PRODUCT_VERSION vendor/rr/config/common.mk | head -1 | awk '{print $3}');
 #This will make TWRP backups name same as ROM zip name
+
+QUARK_ROM_VERSION=RR-O-v$(ROM_VVV)-$(shell date -u +%Y%m%d)-quark-$(RR_BUILDTYPE)
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_DISPLAY_ID=RR-O-v$(ROM_VVV)-$(shell date -u +%Y%m%d)-quark-$(RR_BUILDTYPE)
+    BUILD_DISPLAY_ID=$(QUARK_ROM_VERSION)
+    
+# To make addon SU to work
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.version=$(QUARK_ROM_VERSION)
