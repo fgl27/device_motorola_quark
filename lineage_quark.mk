ad290885ea185162afbd885c0dbb8dde50afa994
Felipe Leon <fglfgl27@gmail.com>
2019-01-10 12:53:20 -0200

    Quark: lineage_quark.mk use $(LOCAL_PATH)
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- lineage_quark.mk
+++ lineage_quark.mk
@@ -1,7 +1,7 @@
 # Inherit some common stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-$(call inherit-product, device/motorola/quark/device.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
