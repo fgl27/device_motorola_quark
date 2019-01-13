a390eaaf2dfb620110d7ea003a254dfd97a078bf
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    Quark: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -95,6 +95,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
