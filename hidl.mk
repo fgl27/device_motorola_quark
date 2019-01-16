fdb2a4b6da874f9dcd3d9d5895845ff1fbace2c0
Felipe Leon <fglfgl27@gmail.com>
2019-01-16 13:03:37 -0200

    Quark: move memtrack to hwbinder
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # IR Blaster
 PRODUCT_PACKAGES += \
