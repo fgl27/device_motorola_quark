f9e0bd4b80270bcdbb925eaea149706290e2049a
Felipe Leon <fglfgl27@gmail.com>
2019-01-16 13:02:32 -0200

    Quark: hidl move keymaster to hwbinder
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -52,7 +52,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
