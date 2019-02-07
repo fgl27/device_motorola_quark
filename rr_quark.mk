1bdaacb3972c53b1315f78c47ce406c128c94799
Felipe Leon <fglfgl27@gmail.com>
2019-02-07 16:17:54 -0200

    Quark: rr_quark.mk fix QUARK_RR_ROM_VERSION
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- rr_quark.mk
+++ rr_quark.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_NAME := rr_quark
 
 #This will make TWRP backups name same as ROM zip name
-QUARK_RR_ROM_VERSION=RR-P-v$(grep PRODUCT_VERSION vendor/rr/build/core/main_version.mk | head -1 | cut -d= -f2 | cut -d' ' -f2)-$(shell date -u +%Y%m%d)-quark-$(RR_BUILDTYPE)
+QUARK_RR_ROM_VERSION=RR-P-v$(shell grep PRODUCT_VERSION vendor/rr/build/core/main_version.mk | head -1 | cut -d= -f2 | cut -d' ' -f2)-$(shell date -u +%Y%m%d)-quark-$(RR_BUILDTYPE)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_DISPLAY_ID=$(QUARK_RR_ROM_VERSION)
