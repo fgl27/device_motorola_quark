561eeee9e5c2df69c928a42a05d28fc8c7ee8606
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    Quark: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -34,6 +34,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += camera2.portability.force_api=1
 
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.charger.enable_suspend=true
+
 # Data Disable mobile data by default
 PRODUCT_PROPERTY_OVERRIDES += ro.com.android.mobiledata=false
 
