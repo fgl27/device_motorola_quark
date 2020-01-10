18b33a83ffc1d30b8183093c6dd36ad1d9366e5b
Michael Bestas <mkbestas@lineageos.org>
2020-01-10 13:58:47 +0100

    Quark: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -78,7 +78,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
