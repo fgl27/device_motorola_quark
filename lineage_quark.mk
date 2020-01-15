27efd9ee95df4ecb241930f853fb3641f5219a5d
fgl27 <fglfgl27@gmail.com>
2020-01-15 17:53:00 -0300

    Quark: update QUARK_LOS_ROM_VERSION
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- lineage_quark.mk
+++ lineage_quark.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_NAME := lineage_quark
 
 #This will make TWRP backups name same as ROM zip name
-QUARK_LOS_ROM_VERSION=lineage-16.0-$(shell date -u +%Y%m%d)-MOD-quark
+QUARK_LOS_ROM_VERSION=lineage-17.1-$(shell date -u +%Y%m%d)-MOD-quark
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_DISPLAY_ID=$(QUARK_LOS_ROM_VERSION)
