f80001746c3e0d1509ad49d13bfbd2240ac7c677
fgl27 <fglfgl27@gmail.com>
2020-01-29 12:48:33 -0300

    Quark: import vibrator service from shamu
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.quark
 
 # Wifi
 PRODUCT_PACKAGES += \
