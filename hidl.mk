2e4146405eddfa1c5a377d3777d08e67b333aac8
YoDevil <ales.astone@gmail.com>
2020-02-07 17:13:34 +0000

    Quark: Move GNSS HIDL HAL to hwbinder
    Change-Id: Id2c15154dc5ba75628023a62e44015ec6271d65a
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -57,7 +57,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # Keymaster
 PRODUCT_PACKAGES += \
