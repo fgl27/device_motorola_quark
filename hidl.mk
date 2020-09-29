7422c7fb93d5dcdbf67875da2248fd047f7d8e03
Bruno Martins <bgcngm@gmail.com>
2020-09-29 10:45:23 +0100

    Quark: Build 2.1 health HAL
    The devices fail to boot otherwise.
    
    Change-Id: I5bc6ae361a5319224b87ef6a4d7127b6b5bc9a97
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -54,9 +54,9 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service \
-    android.hardware.health@2.0-service.override
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # IR Blaster
 PRODUCT_PACKAGES += \
