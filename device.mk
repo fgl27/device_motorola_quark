577d481194ec500e6ad3070c09a8e1ae5f4d691b
Kevin F. Haggerty <haggertk@lineageos.org>
2020-11-28 07:17:09 -0700

    Quark: Use libprotobuf-cpp-lite-v29.so for libwvdrmengine.so
    * libwvdrmengine.so needs _ZN6google8protobuf8internal13empty_string_E
      which is dropped in the API 30 library, but can be found in the
      API 29 prebuilt
    
    Change-Id: I39e3251d53b14f3325282b12e6a6bf741ebcf63a
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -410,3 +410,8 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_PACKAGES += \
     timekeep \
     TimeKeep
+    
+    # VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+
